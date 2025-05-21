60b317e918da3ef544c1d556eea8a259e29c20c4
hakim-stack <aichi.pro@hotmail.com>
2025-05-21 13:58:11 +0200

    Update provider.tf

--- terraform-config/provider.tf
+++ terraform-config/provider.tf
@@ -8,7 +8,27 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
   }
 
   required_version = ">= 1.3.0"
+}
+
+# ➕ Provider Kubernetes pour gérer aws-auth ConfigMap
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# ➕ Datasources pour extraire infos du cluster
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
 }
