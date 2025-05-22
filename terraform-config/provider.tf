b7dc30f69338d234ecff167f885dfad0c6ddf646
hakim-stack <aichi.pro@hotmail.com>
2025-05-22 14:33:08 +0200

    Update provider.tf

--- terraform-config/provider.tf
+++ terraform-config/provider.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.this.name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
 terraform {
   required_providers {
     aws = {
