38b0609382f39f9b538646c5b645280facafedf7
hakim-stack <aichi.pro@hotmail.com>
2025-05-22 16:17:27 +0200

    Update main.tf

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -135,6 +135,33 @@ resource "aws_eks_cluster" "this" {
   }
 }
 
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.project_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"] # tu peux changer le type d'instance ici si besoin
+  ami_type       = "AL2_x86_64"
+  disk_size      = 20
+
+  tags = {
+    Name = "${var.project_name}-node-group"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_readonly_policy
+  ]
+}
+
 resource "aws_eks_access_entry" "build_role_entry" {
   cluster_name  = aws_eks_cluster.this.name
   principal_arn = "arn:aws:iam::116981792309:role/build-kubectl-role"
