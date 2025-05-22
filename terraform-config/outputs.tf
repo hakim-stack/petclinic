b42ba27fe2d45b040e5d4004fb461f708fca6ae5
hakim-stack <aichi.pro@hotmail.com>
2025-05-22 14:33:53 +0200

    Update outputs.tf

--- terraform-config/outputs.tf
+++ terraform-config/outputs.tf
@@ -1,19 +1,19 @@
 output "eks_cluster_name" {
   description = "Nom du cluster EKS"
-  value       = module.eks.cluster_name
+  value       = aws_eks_cluster.this.name
 }
 
 output "eks_cluster_endpoint" {
   description = "Endpoint du cluster EKS"
-  value       = module.eks.cluster_endpoint
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "eks_oidc_provider_arn" {
   description = "ARN du fournisseur OIDC (pour IAM avec service accounts)"
-  value       = module.eks.oidc_provider_arn
+  value       = aws_iam_openid_connect_provider.oidc_provider.arn
 }
 
 output "eks_worker_iam_role_name" {
   description = "Nom du rôle IAM utilisé par les nodes EKS"
-  value       = module.eks.eks_managed_node_groups.default_node_group.iam_role_name
+  value       = aws_iam_role.eks_node_role.name
 }
