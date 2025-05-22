a5c79b750901bdcbe7d380783f2fb8f8fca347b8
hakim-stack <aichi.pro@hotmail.com>
2025-05-22 15:18:02 +0200

    Update variables.tf

--- terraform-config/variables.tf
+++ terraform-config/variables.tf
@@ -40,32 +40,3 @@ variable "availability_zone_2" {
   default     = "eu-west-3b"
 }
 
-variable "node_group_name" {
-  description = "Nom du groupe de nœuds EKS"
-  type        = string
-  default     = "default-node-group"
-}
-
-variable "node_group_instance_types" {
-  description = "Types d'instances EC2 pour les nœuds EKS"
-  type        = list(string)
-  default     = ["t3.medium"]
-}
-
-variable "node_group_desired_size" {
-  description = "Taille désirée du groupe de nœuds"
-  type        = number
-  default     = 2
-}
-
-variable "node_group_min_size" {
-  description = "Taille minimale du groupe de nœuds"
-  type        = number
-  default     = 1
-}
-
-variable "node_group_max_size" {
-  description = "Taille maximale du groupe de nœuds"
-  type        = number
-  default     = 3
-}
