3a0f51ed2cfb3454d70c99365c0927a34668dd4a
hakim-stack <aichi.pro@hotmail.com>
2025-06-18 20:20:08 +0200

    Update main.tf

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -236,16 +236,3 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
-
-resource "aws_route53_record" "cert_validation" {
-  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
-  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
-  zone_id = var.zone_id
-  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
-}
